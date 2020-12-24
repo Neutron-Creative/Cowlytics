bf0726c8de872310bb65faf97260db4bbbf27334
Jim Bisenius <jim@neutroncreative.com>
2020-12-24 08:03:19 -0500

    Created landing page (not responsive)

--- www/layouts/default.vue
+++ www/layouts/default.vue
@@ -1,62 +1,52 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center justify-center w-screen min-h-screen h-auto">
     <Nuxt />
+    <footer class="mt-auto w-full flex flex-row space-x-4 items-center justify-center p-4 lg:p-8 text-center bg-gray-800 text-gray-500 text-sm font-medium">
+      <span>Copyright ©{{ current_year }} Neutron Creative Inc. All rights reserved.</span>
+      <a class="hover:underline" href="#">Terms of Service</a>
+      <a class="hover:underline" href="#">Privacy Policy</a>
+    </footer>
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  @import url('https://rsms.me/inter/inter.css');
+  html, body {
+    @apply overflow-x-hidden;
+  }
+  html {
+    font-family:
+      'Inter',
+      'Source Sans Pro',
+      -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      Roboto,
+      'Helvetica Neue',
+      Arial,
+      sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    line-height: 1.35;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  * {outline:none !important;}
 </style>
+<script>
+export default {
+  data: () => {
+    return {
+    }
+  },
+  computed: {
+    current_year: () => {
+      return new Date().getFullYear();
+    }
+  }
+}
+</script>
--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,78 +1,135 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        www
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <section class="flex flex-col items-center justify-center w-full p-8 lg:py-20 text-center">
+      <div class="flex flex-col w-full max-w-6xl items-center justify-center">
+        <nav class="flex flex-row items-center justify-start w-full max-w-7xl mb-24">
+          <img src="/Icon Bare@2x.png" class="w-8 h-auto mr-3">
+          <span class="text-2xl font-bold text-gray-900">Cowlytics</span>
+        </nav>
+        <h1 class="text-6xl font-extrabold text-gray-900 mb-6">It's time to breakup with Brian</h1>
+        <p class="max-w-4xl text-2xl text-gray-500 mb-6">Choose Cowlytics to experience the freedom of open-source and get back to managing your SaaS analytics how you should - simply.</p>
+        <div class="flex flex-row items-center justify-center mb-12 space-x-4">
+          <a class="shadow px-8 py-5 font-semibold bg-indigo-600 text-white rounded-lg hover:bg-indigo-500 active:ring-4 ring-indigo-600 ring-opacity-50" href="#">Start your 14 day free trial</a>
+          <a class="shadow px-8 py-5 font-semibold bg-gray-100 text-indigo-600 rounded-lg hover:bg-gray-200 active:ring-4 ring-indigo-600 ring-opacity-50" href="#">Sign in</a>
+        </div>
+        <img class="rounded overflow-hidden shadow-lg" src="/Application Screenshot.png"/>
       </div>
-    </div>
+    </section>
+    <section class="flex flex-col items-center justify-center w-screen p-8  text-white lg:py-20 text-center bg-gray-800" style="margin-top:-650px; padding-top: 640px;">
+      <div class="flex flex-col w-full max-w-6xl items-center justify-center">
+        <h3 class="text-gray-400 uppercase tracking-wide font-semibold mb-4">Monitor your SaaS performance from</h3>
+        <div class="flex flex-row items-center space-x-6">
+          <img title="Coming soon" class="w-24 h-auto opacity-30" style="filter:grayscale(1);" src="/shopify.png"/>
+          <img title="Coming soon" class="w-24 h-auto opacity-50" style="filter:grayscale(1);" src="/braintree.svg"/>
+          <img class="w-24 h-auto" src="/stripe.svg"/>
+          <img title="Coming soon" class="w-24 h-auto opacity-50" style="filter:grayscale(1);" src="/chargebee.svg"/>
+          <img title="Coming soon" class="w-24 h-auto opacity-50" style="filter:grayscale(1);" src="/recurly.svg"/>
+        </div>
+      </div>
+    </section>
+    <section class="bg-gray-700 flex flex-col lg:flex-row items-center justify-center w-screen">
+        <div class="flex flex-col lg:flex-row w-full max-w-6xl items-center justify-center">
+          <div class="flex flex-col w-full lg:w-1/2 p-16 border border-gray-600 border-l-0 border-t-0 border-b-0">
+            <img class="w-24 mb-4 opacity-50" style="filter:grayscale(1) invert(1) brightness(100);" src="https://summit-cfo.s3.amazonaws.com/static/images/vendors/chartmogul_logo.svg"/>
+            <div class="relative text-white font-medium leading-7 mb-4">
+              I am in love with Cowlytics. I can't tell you how much it's changed my life. I'm fucking mooing around the house for crying out loud. 
+            </div>
+            <div class="flex flex-row">
+              <img class="rounded-full w-14 mr-4 border-white border-2" src="https://scontent-iad3-1.cdninstagram.com/v/t51.2885-19/s150x150/106238897_725586734946472_5048956344165679204_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_ohc=hIC4pMKn26EAX---DUZ&tp=1&oh=e970e1aa8782ae9d4affd3d199c9adcb&oe=600EB36F"/>
+              <div class="flex flex-col items-start justify-center">
+                <span class="font-semibold text-white mb-1">Jane Doe</span>
+                <span class="text-indigo-100 font-medium">CEO, Company Inc</span>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col w-full lg:w-1/2 p-16">
+            <img class="w-24 mb-4 opacity-50" style="filter:grayscale(1) invert(1) brightness(100);" src="https://summit-cfo.s3.amazonaws.com/static/images/vendors/chartmogul_logo.svg"/>
+            <div class="relative text-white font-medium leading-7 mb-4">
+              I am in love with Cowlytics. I can't tell you how much it's changed my life. I'm fucking mooing around the house for crying out loud. 
+            </div>
+            <div class="flex flex-row">
+              <img class="rounded-full w-14 mr-4 border-white border-2" src="https://scontent-iad3-1.cdninstagram.com/v/t51.2885-19/s150x150/106238897_725586734946472_5048956344165679204_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_ohc=hIC4pMKn26EAX---DUZ&tp=1&oh=e970e1aa8782ae9d4affd3d199c9adcb&oe=600EB36F"/>
+              <div class="flex flex-col items-start justify-center">
+                <span class="font-semibold text-white mb-1">Jane Doe</span>
+                <span class="text-indigo-100 font-medium">CEO, Company Inc</span>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+    <section class="flex flex-col items-center justify-center w-full p-8 lg:py-32 text-center">
+      <div class="flex flex-col w-full max-w-6xl items-center justify-center">
+        <h2 class="text-5xl font-extrabold text-gray-900 mb-6">Simple fixed pricing</h2>
+        <p class="max-w-4xl text-xl text-gray-500 mb-12">Import your Stripe account, invite your team, and get going in seconds.</p>
+        <div class="flex flex-row w-full rounded-lg shadow-lg overflow-hidden mb-32">
+          <div class="flex flex-col w-full lg:w-3/4 p-12 text-left">
+            <h3 class="text-2xl font-bold mb-2 text-gray-900 w-full">Everything included for one fixed price.</h3>
+            <p class="text-lg leading-relaxed text-gray-500 mb-6">At Cowlytics, we belive pricing should be simple and scale friendly. That's why Cloudlytics Cloud is only $30/Month, and Cloudlytics self-hosted is forever free.</p>
+            <div class="flex flex-row items-center justify-center w-full mb-6">
+              <h4 class="uppercase tracking-wide text-indigo-600 font-semibold mr-2">Everything included</h4>
+              <div style="height:2px;" class="flex flex-grow bg-indigo-600 opacity-50"></div>
+            </div>
+            <div class="flex flex-row items-center justify-start w-full mb-6">
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+               Customizable dashboard
+              </div>
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+                Subscription metrics
+              </div>
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+                Financial forecasts
+              </div>
+            </div>
+            <div class="flex flex-row items-center justify-start w-full">
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+                Unlimited revenue
+              </div>
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+                Unlimited team members
+              </div>
+              <div class="flex flex-row items-center justify-start w-full lg:w-1/3 font-medium text-gray-500">
+                <img src="/Checkmark Filled Purple.svg" class="w-4 opacity-50 mr-2"/>
+                Unlimited reports
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col flex-grow bg-gray-100 w-full lg:w-1/4 p-8 items-center justify-center">
+            <span class="text-gray-500 font-medium mb-3">Starting at</span>
+            <div class="flex flex-row items-center justify-center mb-3">
+              <span class="font-extrabold text-5xl text-gray-900">$30</span>
+              <span class="text-gray-500 font-medium ml-2">USD</span>
+            </div>
+            <span class="text-gray-500 font-medium text-sm mb-6">per month</span>
+            <span class="text-xs text-gray-500 mb-6">Unlimited everything, no catch.</span>
+            <a class="text-sm font-semibold w-full text-white bg-indigo-600 hover:bg-indigo-500 text-center py-4 px-6 rounded-lg" href="#">Start your free trial</a>
+          </div>
+        </div>
+        <div class="flex flex-col w-full text-left">
+          <h2 class="text-3xl font-extrabold text-gray-900 py-6 border-2 border-gray-200 border-t-0 border-l-0 border-r-0 w-full text-left">Frequently asked questions</h2>
+          <div class="flex flex-row border border-gray-200 border-l-0 border-t-0 border-r-0">
+            <div class="flex flex-col w-full lg:w-1/3 p-8 font-medium text-gray-800">
+              How does Cowlytics work?
+            </div>
+            <div class="flex flex-col w-full lg:w-2/3 p-8 text-gray-500 leading-relaxed">
+              Cloudlytics Cloud is a simple & easy to use product that requires little-no configuration on your end to get up and running. To being, create your free account and link your Stripe through the onboarding as follows. Afterwhich, enjoy your new connected profile!
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="flex flex-col items-center justify-center w-full p-8 lg:py-24 text-center bg-indigo-100">
+      <div class="flex flex-col lg:flex-row w-full max-w-6xl items-center justify-center">
+        <div class="flex flex-col flex-grow text-left">
+          <h2 class="text-5xl font-extrabold text-gray-900 mb-6">Ready to get started?</h2>
+          <p class="text-lg text-gray-500">Import your Stripe account, invite your team, and get going in seconds.</p>
+        </div>
+        <a class="text-sm font-semibold text-white bg-indigo-600 hover:bg-indigo-500 text-center py-4 px-6 rounded-lg" href="#">Start your free trial</a>
+      </div>
+    </section>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
